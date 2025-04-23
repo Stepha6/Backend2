d65d99eab57bda0031be42b6bf5625d7a23f12d0
Stepha6 <s68106680@gmail.com>
2025-04-22 21:57:31 -0500

    Create connection.py

---
+++ connection.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+
+
+def connect_to_mongodb(db_name, collection_name):
+    uri = "mongodb+srv://s68106680:Tefa2006@cluster0.4o7he.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    client = MongoClient(uri, server_api=ServerApi('1'))
+    db = client[db_name]
+    collection = db[collection_name]
+    return collection
