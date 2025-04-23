ae00c663ef8485ebe89bb7d5d321437bff394e6e
Stepha6 <s68106680@gmail.com>
2025-04-22 22:49:22 -0500

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,5 @@
+# auth.py
+def verificar_farmaceutico(user_id: str) -> bool:
+    # Lógica para verificar al farmacéutico
+    # Devuelve True si la verificación es exitosa, de lo contrario, False
+    return True  # O alguna lógica real
